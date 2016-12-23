850462d7306cf1965a79f99f5ace4cc7e7946ac1
Adrian Podlawski <apodlawski@chromecrm.com>
2016-12-23 11:23:18 +0100

    Module 1

---
+++ CoreApp/src/CoreApp/Greeter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApp
+{
+    public interface IGreeter {
+        string GetGreeting();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private string _greeting;
+
+        public Greeter(IConfiguration configuration) { //IConfiguration which I created and where I store my configuration
+            _greeting = configuration["Greeting"];
+        }
+        public string GetGreeting() {
+            return _greeting;
+        }
+    }
+}
--- CoreApp/src/CoreApp/Startup.cs
+++ CoreApp/src/CoreApp/Startup.cs
@@ -7,19 +7,33 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace CoreApp
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env) {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)//If we dont need to hardcode we can Pass IHostingEnvironment so it will set our root Path on local and server also
+                .AddJsonFile("appsettings.json")//add configuration parameter
+                .AddEnvironmentVariables(); //when we want to ask configuration parameter later.
+            //we can add more confiugration sources
+
+           Configuration = builder.Build(); // build configuration and set acces to this configuration
+        }
+        public IConfiguration Configuration { get; set; }//Configuration property for acces
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeter, Greeter>();  //if you see something that requires iGreeter, use class Greeter
+            services.AddSingleton(Configuration); //type of IConfiguration, so ASP.NET knows, when to use Configuration parameter
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IGreeter greeter)
         {
             loggerFactory.AddConsole();
 
@@ -30,7 +44,8 @@ namespace CoreApp
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var message = greeter.GetGreeting(); //<----------Service is required
+                await context.Response.WriteAsync(message);
             });
         }
     }
